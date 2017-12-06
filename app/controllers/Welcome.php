3f010ea14f08c6e855e13bd85832167cfc378987
lidi <lidi0903@163.com>
2017-12-06 10:57:50 +0800

    实现路由加载

---
+++ app/controllers/Welcome.php
@@ -0,0 +1,12 @@
+<?php
+class Welcome
+{
+	function __construct()
+	{
+		# code...
+	}
+
+	public function index(){
+		echo 111;
+	}
+}
--- system/core/FavCI.php
+++ system/core/FavCI.php
@@ -6,9 +6,37 @@ $URI =& load_class('URI', 'core');
 
 $RTR =& load_class('Router', 'core');
 
-$e404 = false;
+$e404 = FALSE;
 $class = ucfirst($RTR->class);
 $method = $RTR->method;
-echo $class.'|'.$method;
 
+$reqFile = APPPATH . 'controllers'. DIRECTORY_SEPARATOR . $RTR->directory.$class.'.php';
+if(empty($class) OR !file_exists($reqFile)){
+	$e404 = TRUE;
+}else{
+	require_once($reqFile);
+	if ( ! class_exists($class, FALSE) OR $method[0] === '_' OR method_exists('FavCI_Controller', $method))
+	{
+		$e404 = TRUE;
+	}
+	elseif ( ! method_exists($class, $method))
+	{
+		$e404 = TRUE;
+	}
+	elseif ( ! is_callable(array($class, $method)))
+	{
+		$reflection = new ReflectionMethod($class, $method);
+		if ( ! $reflection->isPublic() OR $reflection->isConstructor())
+		{
+			$e404 = TRUE;
+		}
+	}
+}
 
+if($e404){
+	exit('Not Found!');
+}
+
+$param = ['Hello'];
+$FavCI = new $class();
+call_user_func_array([&$FavCI, $method], $param);
