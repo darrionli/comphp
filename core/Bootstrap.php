8476418570793c5a4ff998b4dc5220227212c450
lidi <lidi0903@163.com>
2018-03-21 18:19:38 +0800

    route

--- core/Bootstrap.php
+++ core/Bootstrap.php
@@ -1,15 +1,21 @@
 <?php
 namespace core;
 
+use core;
+
 class Bootstrap
 {
 	public static function run()
 	{
-		self::parseUri();
+		Uri::parseUrl();
+		self::parseUrl();
 	}
 
-	// 拆分URL
-	public static function parseUri()
+	/**
+	 * 解析REQUEST_URI并自动检测URI
+	 * 必要时修复查询字符串
+	 */
+	public static function parseUrl()
 	{
 		$request = $_SERVER['REQUEST_URI'];
 		$segment = explode("/", $request);
@@ -20,4 +26,48 @@ class Bootstrap
 		// (new $class)->$method();
 		call_user_func([(new $class), $method]);
 	}
+
+	// 解析URI，并修复查询字符串
+	public static function parseUri()
+	{
+		if ( ! isset($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']))
+		{
+			return '';
+		}
+
+		$uri = parse_url('http://dummy' . $_SERVER['REQUEST_URI']);
+		$query = isset($uri['query']) ? $uri['query'] : '';
+		$path = isset($uri['path']) ? $uri['path'] : '';
+
+		if (isset($_SERVER['SCRIPT_NAME'][0]))
+		{
+			if (strpos($path, $_SERVER['SCRIPT_NAME']) === 0)
+			{
+				$path = (string) substr($path, strlen($_SERVER['SCRIPT_NAME']));
+			}
+			elseif (strpos($path, dirname($_SERVER['SCRIPT_NAME'])) === 0)
+			{
+				$path = (string) substr($path, strlen(dirname($_SERVER['SCRIPT_NAME'])));
+			}
+		}
+
+		if (trim($path, '/') === '' && strncmp($query, '/', 1) === 0)
+		{
+			$query = explode('?', $query, 2);
+			$path = $query[0];
+			$_SERVER['QUERY_STRING'] = isset($query[1]) ? $query[1] : '';
+		}
+		else
+		{
+			$_SERVER['QUERY_STRING'] = $query;
+		}
+
+		parse_str($_SERVER['QUERY_STRING'], $_GET);
+
+		if ($path === '/' OR $path === '')
+		{
+			return '/';
+		}
+	}
+
 }
---
+++ core/Uri.php
@@ -0,0 +1,11 @@
+<?php
+namespace core;
+
+class Uri
+{
+	// 解析并修复URL
+	public static function parseUrl()
+	{
+		echo 111;
+	}
+}
